77005dbc15f3ebe891647e0969746c74d867de70
Jhon Jairo Hernandez Pulgarin <jhon@appenate.com>
2024-10-08 16:20:16 -0500

    inicio

---
+++ src/app/interfaces/iTareaConUsuarioDTO.ts
@@ -0,0 +1,9 @@
+export interface iTareaConUsuarioDTO{
+    tareaId: number,
+    descripcion: string,
+    estado: string,
+    usuarioId: number,
+    usuarioNombre: string,
+    proyectoId: number,
+    proyectoNombre: string
+}
